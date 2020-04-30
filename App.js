217819f9045955eb5992fc22004c38e694abbb47
FPLCodes <59481822+FPLCodes@users.noreply.github.com>
2020-04-30 23:49:24 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Quote from "./QuoteGen/Quote";
+
+function App() {
+  return (
+    <div className="App">
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
